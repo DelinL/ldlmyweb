f0b2ac202945dfcfacc24d92acee5e96cda8e1c3
delin <852212583@qq.com>
2018-07-05 18:51:03 +0800

    createReadStream

--- app/index.js
+++ app/index.js
@@ -1,14 +1,18 @@
 // 主要核心逻辑入口
 
 var fs = require('fs');
+var path = require('path');
+
+var root = path.resolve(process.argv[2] || '.');
 
 var App = function(){};
 App.prototype.initServer = function(req, res) {
-
+    res.writeHead(200);
 	//.readFile第一个参数是文件路径，./是相对于process.cwd()
-	fs.readFile('./public/index.html', 'utf8', function(err, data){
-		res.end(data);
-	})
+	var filepath = path.join(root,'public', 'index.html');
+	fs.createReadStream(filepath).pipe(res);
+
+	console.log('Static root dir: ' + root);
 }
 
 module.exports = App
