99b56a2bd2f864314d6c61162dced13ec3938aa4
delin Lu 2018PC <ludelin1992@qq.com>
2018-07-08 06:02:21 +0800

    response writeHead

--- app/index.js
+++ app/index.js
@@ -7,7 +7,7 @@ var root = path.resolve(process.argv[2] || '.');
 
 var App = function(){};
 App.prototype.initServer = function(req, res) {
-    res.writeHead(200);
+    res.writeHead(200, {'Content-Type': 'text/html'});
 	//.readFile第一个参数是文件路径，./是相对于process.cwd()
 	var filepath = path.join(root,'public', 'index.html');
 	fs.createReadStream(filepath).pipe(res);
